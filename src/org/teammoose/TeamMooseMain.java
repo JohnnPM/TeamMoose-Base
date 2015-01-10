092f358a28fbac8acc79c6caa9bd271fd84edc69
xXJarJarXx <598Johnn897@gmail.com>
2015-01-10 08:46:57 -0500

    boop

---
+++ src/org/teammoose/TeamMooseMain.java
@@ -0,0 +1,49 @@
+/*
+ * Author: 598Johnn897
+ * 
+ * Date: Jan 8, 2015
+ * Package: org.teammoose
+ */
+package org.teammoose;
+
+import org.apache.commons.lang.Validate;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * 
+ * @author 598Johnn897
+ * @since
+ */
+public class TeamMooseMain extends JavaPlugin
+{
+	public static TeamMooseMain instance;
+	public static TeamMooseMain get()
+	{
+		Validate.notNull(instance);
+		return instance;
+	}
+	
+	@Override public void onLoad()
+	{
+		
+	}
+	
+	@Override public void onEnable()
+	{
+		instance = this;
+		try
+		{
+			
+		} catch(Exception e)
+		{
+			e.printStackTrace();
+		} finally {
+			
+		}
+	}
+	
+	@Override public void onDisable()
+	{
+		
+	}
+}
---
+++ src/org/teammoose/framework/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * Author: 598Johnn897
+ * 
+ * Date: Jan 8, 2015
+ * Package: org.teammoose.framework
+ */
+/**
+ * 
+ * @author 598Johnn897
+ * @since
+ */
+package org.teammoose.framework;
